d2bbd07ae3fe44c448c2247d846d364d3c3e6206
ozanazizoglu <azizogluozan@hotmail.com>
2021-02-08 20:33:09 +0300

    Add files via upload

---
+++ source/src/App.js
@@ -0,0 +1,13 @@
+import './css/App.css';
+import React from 'react'
+import Main from './pages'
+import { Switch, Route } from 'react-router-dom'
+function App() {
+  return (
+    <Switch>
+      <Route path = "/" component = {Main}></Route>
+    </Switch>
+  );
+}
+
+export default App;
